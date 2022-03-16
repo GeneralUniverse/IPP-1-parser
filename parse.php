4eb548139e8291c1e54e6762cb35dca38f853a90
Dominik <dominik.klon@outlook.com>
2022-03-16 18:18:50 +0100

    added some more checks like read f.e.

--- parse.php
+++ parse.php
@@ -18,12 +18,13 @@ checkTheHeader($stdin);
 $xml->startElement("program");
 $xml->writeAttribute("language","IPPcode22");
 
-for ($i=0;($line = fgets($stdin)) !== false;$i++) {
+for ($i=1;($line = fgets($stdin)) !== false;$i++) {
 
     $words = lineToProperArray($line, $stdin);
 
     if($words[0]==""){ // we are not working with empty lines
-        break;
+        $i--;
+        continue;
     }
 
     $xml->startElement("instruction");
@@ -43,7 +44,6 @@ for ($i=0;($line = fgets($stdin)) !== false;$i++) {
         case "CALL":
         case "DEFVAR":
             checkNumbArg($words,1);
-
             createArgument($xml, "arg1", "var", $words[1]);
             break;
 
@@ -62,9 +62,14 @@ for ($i=0;($line = fgets($stdin)) !== false;$i++) {
             break;
 
         case "READ":
+            checkNumbArg($words,2);
+            createArgument($xml, "arg1", "var", $words[1]);
+            createArgument($xml, "arg1", "type", $words[2]);
+            break;
         case "STRLEN":
         case "TYPE":
         case "MOVE":
+        case "NOT":
             checkNumbArg($words,2);
             createArgument($xml, "arg1", "var", $words[1]);
             createArgument($xml, "arg2", "sym", $words[2]);
@@ -79,7 +84,6 @@ for ($i=0;($line = fgets($stdin)) !== false;$i++) {
         case "EQ":
         case "AND":
         case "OR":
-        case "NOT":
         case "STRI2INT":
         case "CONCAT":
         case "GETCHAR":
@@ -98,7 +102,7 @@ for ($i=0;($line = fgets($stdin)) !== false;$i++) {
             createArgument($xml, "arg3", "sym", $words[3]);
             break;
         default:
-            // exit(22);
+            exit(22);
     }
     $xml->endElement(); // instruction element end
 }
@@ -142,6 +146,9 @@ function getContent($arg){
         return substr($arg, 5);
     }
     elseif(strpos($arg,"nil@") === 0) {
+        if(substr($arg, 4) != "nil"){ //only value for nil is nil
+            exit(23);
+        }
         return "nil";
     }
     else{
@@ -157,11 +164,23 @@ function checkNumbArg($words,$corrNum){
 }
 
 function checkTheType($argType,$content){
+    if($argType == "type"){
+        $content = strtoupper($content);
+        if($content != "INT" && $content != "STRING" && $content != "BOOL"){
+            exit(23);
+        }
+        else{
+            return;
+        }
+    }
     if($argType == "label" && getArgType($content) == "label"){
         return;
     }
     if($argType != "sym" && $argType != getArgType($content)){
         exit(23);
+    }
+    if($argType == "sym" && (strpos($content,"@") == false)){ //if its label in sym then exit
+        exit (23);
     }
 }
 
@@ -177,8 +196,11 @@ function createArgument ($xml, $argName, $argType, $content){
         $argType == "var";
     }
 
+    if($argType == "type"){
+        $argType = $content;
+    }
+
     $content = getContent($content);
-    //if var type, then var content
 
     $xml->startElement($argName);
     $xml->writeAttribute("type", $argType);
@@ -190,13 +212,17 @@ function createArgument ($xml, $argName, $argType, $content){
 function lineToProperArray($line, $stdin)
 {
     $line = commentIgnore($line);
-    $line=trim($line);
+    $line = htmlspecialchars($line);
+    $line = trim($line);
+
     //if its empty line, read the next line
-    if($line == "" || $line == "\n"){
+    if($line == ""){
         $line = fgets($stdin);
         $line = commentIgnore($line);
+        $line = htmlspecialchars($line);
+        $line = trim($line);
     }
-    $line=trim($line);
+
     $words = explode(" ",$line);
     $words[0] = strtoupper($words[0]);
 
@@ -205,16 +231,31 @@ function lineToProperArray($line, $stdin)
 
 /**Function check if stdin has the proper header*/
 function checkTheHeader($stdin){
-    if(fgets($stdin)!=".IPPcode22\n"){
-        exit("21\n");
+    $words = lineToProperArray(fgets($stdin),$stdin);
+
+    if(count($words)>1){
+        exit(21);
     }
+
+    if ($words[0] == ".IPPCODE22") {
+        return;
+    }
+
+    while($words[0] == "\n" || $words[0] == "") {
+        $words = lineToProperArray(fgets($stdin),$stdin);
+        if ($words[0] == ".IPPCODE22") {
+            return;
+        }
+    }
+
+    exit (21);
 }
 
 /**Function set the necessary XML properties and write the XML header.*/
 function setStartXML($xml){
-    $xml->openUri('php://output');
-    xmlwriter_set_indent($xml, 4);
-    xmlwriter_set_indent_string($xml, "\t");
+    $xml -> openURI("php://stdout");
+    xmlwriter_set_indent($xml, "1");
+    xmlwriter_set_indent_string($xml, " ");
     $xml->startDocument('1.0','utf-8');
 }
 
@@ -247,7 +288,13 @@ function commentIgnore($line)
 {
     $i=0;
     $commentFreeLine="";
-    $letter=$line[0];
+
+    if(!$line){
+        return $commentFreeLine; //explode function returns error with empty string
+    }
+
+    $letter = $line[0];
+
     while($letter!='#' && $i<strlen($line)){
         $letter=$line[$i];
         if($letter=='#'){
@@ -263,5 +310,10 @@ function commentIgnore($line)
 
 /**Function prints help for users.*/
 function printHelp(){
+//    $str="/032";
+//    $str = preg_replace_callback('/\\\([0-9]{3})/', function ($match) {
+//        return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+//    }, $str);
+//    echo $str;
     echo "POMOOOOOC\n";
 }
